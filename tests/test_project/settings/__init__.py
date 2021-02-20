1fffeb7016365ed682af09e7426e5edcec1a277d
Thu Trang Pham <thu@joinmodernhealth.com>
2021-02-20 08:52:11 -0800

    feat(ISSUE-8): Formatting m2m confirmation values as html lists

--- tests/test_project/settings/__init__.py
+++ tests/test_project/settings/__init__.py
@@ -1,1 +1,6 @@
-from .local import *  # noqa
+# For some reason the working directory of tests
+# And running the server are different
+# (Possibly due to test_project being within a subfolder)
+# This defaults settings to local unless
+# DJANGO_SETTINGS is specified.
+from .local import *
