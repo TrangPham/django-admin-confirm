8fe2a5e21f86cb81d95c541304ba38da7453be9a
Thu Trang Pham <thu@joinmodernhealth.com>
2021-02-23 20:31:18 -0800

    More tests

--- admin_confirm/tests/test_integration_confirm_save_actions.py
+++ admin_confirm/tests/test_integration_confirm_save_actions.py
@@ -105,10 +105,6 @@ class ConfirmSaveActionsIntegrationTestCases(AdminConfirmTestCase):
         self.assertEqual(cached_item.price, data["price"])
         self.assertEqual(cached_item.currency, data["currency"])
 
-        # cached_change_message = cache.get(CACHE_KEYS["change_message"])
-        # self.assertIsNotNone(cached_change_message)
-        # self.assertIn("changed", cached_change_message[0].keys())
-
         # Should not have saved the changes yet
         self.assertEqual(Item.objects.count(), 1)
         item.refresh_from_db()
@@ -354,11 +350,6 @@ class ConfirmSaveActionsIntegrationTestCases(AdminConfirmTestCase):
         cached_item = cache.get(CACHE_KEYS["object"])
         self.assertIsNotNone(cached_item)
         self.assertIsNone(cached_item.id)
-
-        # cached_change_message = cache.get(CACHE_KEYS["change_message"])
-        # self.assertIsNotNone(cached_change_message)
-        # self.assertIn("added", cached_change_message[0].keys())
-
         # Should not have saved the object yet
         self.assertEqual(ShoppingMall.objects.count(), 0)
 
