b0428931eeefad802ff78a331818b6a4716dcf2f
Thu Trang Pham <thu@joinmodernhealth.com>
2021-02-24 23:27:57 -0800

    Got first integration test to work

--- admin_confirm/tests/integration/test_smoke.py
+++ admin_confirm/tests/integration/test_smoke.py
@@ -1,18 +1,24 @@
+import socket
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class FunctionalTestCase(LiveServerTestCase):
-    host = 'web'
-    def setUp(self):
-        self.browser = webdriver.Remote(
+    @classmethod
+    def setUpClass(cls):
+        cls.host = socket.gethostbyname(socket.gethostname())
+        cls.selenium = webdriver.Remote(
             command_executor="http://selenium:4444/wd/hub",
-            desired_capabilities=DesiredCapabilities.FIREFOX
+            desired_capabilities=DesiredCapabilities.FIREFOX,
         )
+        super().setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super().tearDownClass()
+
 
     def test_user_registration(self):
-        self.browser.get(self.live_server_url)
-        self.assertIn('Django', self.browser.title)
-
-    def tearDown(self):
-        self.browser.close()
+        self.selenium.get(self.live_server_url+'/admin/')
+        self.assertIn('Django', self.selenium.title)
