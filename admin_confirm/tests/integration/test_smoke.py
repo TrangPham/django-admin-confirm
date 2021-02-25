cc171cdc4dc6a3eccd6152fee09f6b9878174d6e
Thu Trang Pham <thu@joinmodernhealth.com>
2021-02-24 23:32:23 -0800

    Refactor a bit

--- admin_confirm/tests/integration/test_smoke.py
+++ admin_confirm/tests/integration/test_smoke.py
@@ -1,24 +1,7 @@
-import socket
-from django.test import LiveServerTestCase
-from selenium import webdriver
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-
-class FunctionalTestCase(LiveServerTestCase):
-    @classmethod
-    def setUpClass(cls):
-        cls.host = socket.gethostbyname(socket.gethostname())
-        cls.selenium = webdriver.Remote(
-            command_executor="http://selenium:4444/wd/hub",
-            desired_capabilities=DesiredCapabilities.FIREFOX,
-        )
-        super().setUpClass()
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.selenium.quit()
-        super().tearDownClass()
+from admin_confirm.tests.helpers import AdminConfirmIntegrationTestCase
 
 
-    def test_user_registration(self):
+class SmokeTest(AdminConfirmIntegrationTestCase):
+    def test_load_admin(self):
         self.selenium.get(self.live_server_url+'/admin/')
         self.assertIn('Django', self.selenium.title)
