432bc17928d748e80695743ac80246f77ab3ab7c
Thu Trang Pham <thuutrangpham@gmail.com>
2021-06-20 19:09:00 -0700

    Update create bucket script to output commands

--- docker-entrypoint-initaws.d/create_bucket.sh
+++ docker-entrypoint-initaws.d/create_bucket.sh
@@ -1,3 +1,8 @@
-aws configure set aws_access_key_id test
-aws configure set aws_secret_access_key test
+#!/bin/bash
+set -x
 awslocal s3 mb s3://mybucket
+set +x
+
+# aws configure set aws_access_key_id test
+# aws configure set aws_secret_access_key test
+# awslocal s3 mb s3://mybucket
