d46b52a8f31050b96df576bed3b1c56428671d10
Thu Trang Pham <thuutrangpham@gmail.com>
2021-06-21 07:54:15 -0700

    Try some other stuff

--- conftest.py
+++ conftest.py
@@ -9,5 +9,8 @@ def pytest_sessionstart(session):
     session = localstack_client.session.Session(region_name="us-west-1")
     s3 = session.resource("s3")
     print("Creating s3 bucket named: mybucket")
-    s3.create_bucket(Bucket="mybucket")
+    try:
+        s3.create_bucket(Bucket="mybucket")
+    except Exception as e:
+        print(f"Encountered an exception: {str(e)}")
     print("DONE")
