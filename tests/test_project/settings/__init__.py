66871b3c84a81517d34a3da27c5f705f56689354
Thu Trang Pham <thuutrangpham@gmail.com>
2022-07-07 09:28:04 -0700

    Fix tests loading AppConfig

--- tests/test_project/settings/__init__.py
+++ tests/test_project/settings/__init__.py
@@ -2,5 +2,5 @@
 # And running the server are different
 # (Possibly due to test_project being within a subfolder)
 # This defaults settings to local unless
-# DJANGO_SETTINGS is specified.
+# DJANGO_SETTINGS_MODULE is specified.
 from .local import *
