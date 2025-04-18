728f99df019c97811e4d1e84bb2749e79ae8e1a6
Thu Trang Pham <thu.pham@mass.gov>
2025-04-18 12:07:13 -0700

    Drop support for python 3.7

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.2",
         "Framework :: Django :: 5.2",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
